19ff5cff41d114255f2326759d5bf8a5a1541d37
kleij <kleijahaj08@gmail.com>
2023-05-20 21:49:20 +0200

    committed

---
+++ View/Admin/index.php
@@ -0,0 +1,47 @@
+<?php
+include "../../Model/connection.php";
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Admin Dashboard</title>
+  <link rel="stylesheet" href="../../Asset/CSS/admin.css">
+</head>
+<body>
+  <div class="header">
+    <h1 id="welcome">Admin Dashboard</h1>
+    <div class="user-profile">
+      <span>Username</span>
+      <div class="dropdown-menu">
+        <ul>
+          <li><a href="youtube.com">Profile</a></li>
+          <li>Settings</li>
+          <li>Logout</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  
+  <div class="sidebar">
+    <ul>
+      <li><a href="index.html">Dashboard</a></li>
+      <li><a href="stockcheck.html">Check stock</a></li>
+      <li>Buy stock</li>
+      <li>Check requests</li>
+    </ul>
+  </div>
+  
+  <div class="main-content">
+    <h2 class="content-heading">Welcome to the Dashboard</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed volutpat 
+        tellus at elit finibus dapibus. Duis luctus tristique orci, id phare
+        tra mauris fermentum nec. Sed efficitur cursus rutrum. Sed vulputate
+         elit ut lacinia consectetur.</p>
+  </div>
+  
+  <div class="footer">
+    <p>&copy; 2023 Admin Dashboard. All rights reserved.</p>
+  </div>
+</body>
+</html>
