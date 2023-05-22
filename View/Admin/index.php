436abc1f82a8cfbea04bd3d682a8c5732c9bcc1f
agoga21 <rurunikenshin1@gmail.com>
2023-05-22 19:38:24 +0200

    committed

--- View/Admin/index.php
+++ View/Admin/index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html>
+
 <head>
   <title>Admin Dashboard</title>
   <link rel="stylesheet" href="../../Asset/CSS/admin.css">
@@ -7,61 +8,65 @@
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" crossorigin="anonymous" />
 
 </head>
+
 <body>
 
-<div class="sidebar" id='sidebar'>
-  <div class="headeri">
-     <button type="button" class="toggle" id="toggle">
-       <span></span> 
+  <div class="sidebar" id='sidebar'>
+    <div class="headeri">
+      <button type="button" class="toggle" id="toggle">
+        <span></span>
       </button>
-      <div class="admin-dashboard"> <h1>Admin Dashboard</h1>
-            </div><div class="header">
-              <div class="user-profile">
-                  <span>Username</span>
-                    <div class="dropdown-menu">
-                      <ul>
-                        <li><a href="#">Profile</a></li>
-                        <li><a href="#">Settings</a></li>
-                        <li><a href="#">Logout</a></li>
-                      </ul>
-                    </div>
-              </div>
+      <div class="admin-dashboard">
+        <h1>Admin Dashboard</h1>
+      </div>
+      <div class="header">
+        <div class="user-profile">
+          <span>Username</span>
+          <div class="dropdown-menu">
+            <ul>
+              <li><a href="#">Profile</a></li>
+              <li><a href="#">Settings</a></li>
+              <li><a href="#">Logout</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+    <ul>
+      <li><a href="#"><i class="fa fa-home" aria-hidden="true"></i> Dashboard</a></li>
+      <br>
+      <li><a href="#"><i class="fas fa-clipboard-check" aria-hidden="true"></i> Check Stock</a></li>
+      <br>
+      <li><a href="#"><i class="fas fa-money-bills" aria-hidden="true"></i> Buy Stock</a></li>
+      <br>
+      <li><a href="#"><i class="fas fa-receipt" aria-hidden="true"></i> Check Requests</a></li>
+      <br>
+    </ul>
   </div>
-  </div>
-  <ul>
-      <li><a href="#"><i class="fa fa-home" aria-hidden="true"></i>  Dashboard</a></li>
-      <br>
-      <li><a href="#"><i class="fas fa-clipboard-check" aria-hidden="true"></i>  Check Stock</a></li>
-      <br>
-			<li><a href="#"><i class="fas fa-money-bills" aria-hidden="true"></i>  Buy Stock</a></li>
-      <br>
-			<li><a href="#"><i class="fas fa-receipt" aria-hidden="true"></i>  Check Requests</a></li>
-      <br>
-  </ul>
-</div>
-  
-    <footer id="footer">
-      <p>&copy; 2023 Admin Dashboard. All rights reserved.</p>
-    </footer>
-  
+
+  <footer id="footer">
+    <p>&copy; 2023 Admin Dashboard. All rights reserved.</p>
+  </footer>
+
   <script>
     var btn = document.querySelector('.toggle');
-var btnst = true;
-btn.onclick = function() {
-  if(btnst == true) {
-    document.querySelector('.toggle span').classList.add('toggle');
-    document.getElementById('sidebar').classList.add('sidebarshow');
-    btnst = false;
-  }else if(btnst == false) {
-    document.querySelector('.toggle span').classList.remove('toggle');
-    document.getElementById('sidebar').classList.remove('sidebarshow');
-    btnst = true;
-  }
-}
+    var btnst = true;
+    btn.onclick = function() {
+      if (btnst == true) {
+        document.querySelector('.toggle span').classList.add('toggle');
+        document.getElementById('sidebar').classList.add('sidebarshow');
+        btnst = false;
+      } else if (btnst == false) {
+        document.querySelector('.toggle span').classList.remove('toggle');
+        document.getElementById('sidebar').classList.remove('sidebarshow');
+        btnst = true;
+      }
+    }
   </script>
-  <script rel ="script" href="insertStockContr.js">
-        
-</script>
+  <script rel="script" href="insertStockContr.js">
+
+  </script>
 
 </body>
+
 </html>
