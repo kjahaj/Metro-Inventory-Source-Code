bf3b2e111a3cc3f404d78c4a6cf909fa54267547
kleij <kleijahaj08@gmail.com>
2023-05-26 19:49:59 +0200

    committed

--- Controller/show-stock.js
+++ Controller/show-stock.js
@@ -1,32 +1,58 @@
 document.addEventListener("DOMContentLoaded", function() {
-    fetchData();
-  });
-  
-  function fetchData() {
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", "../../Model/show-stock.php", true);
-    xhr.onreadystatechange = function() {
-      if (xhr.readyState === 4 && xhr.status === 200) {
-        console.log("sss");
-        var data = JSON.parse(xhr.responseText);
-        
-        // Display the data in a table
-        var table = document.getElementById("data-table");
-        for (var i = 0; i < data.length; i++) {
-          var row = table.insertRow(i + 1); // Add rows after the header row
-          
-          var itemCell = row.insertCell(1);
-          var categoryCell = row.insertCell(2);
-          var quantityCell = row.insertCell(3);
-          var warehouseCell = row.insertCell(4);
-          
-          itemCell.textContent = data[i].item;
-          categoryCell.textContent = data[i].category;
-          quantityCell.textContent = data[i].quantity;
-          warehouseCell.textContent = data[i].warehouse;
-        }
+  fetchData();
+});
+
+function fetchData() {
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", "../../Model/show-stock.php", true);
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      var data = JSON.parse(xhr.responseText);
+
+      // Display the data in a table
+      var table = document.getElementById("data-table");
+      for (var i = 0; i < data.length; i++) {
+        var row = table.insertRow(i + 1); // Add rows after the header row
+
+        var itemCell = row.insertCell(0);
+        var categoryCell = row.insertCell(1);
+        var quantityCell = row.insertCell(2);
+        var warehouseCell = row.insertCell(3);
+        var buttonCell = row.insertCell(4); // Add a cell for the button
+
+        itemCell.textContent = data[i].item;
+        categoryCell.textContent = data[i].category;
+        quantityCell.textContent = data[i].quantity;
+        warehouseCell.textContent = data[i].warehouse;
+
+        // Create a button element
+        var button = document.createElement("button");
+        button.textContent = "Click";
+        button.addEventListener("click", createButtonClickHandler(data[i])); // Attach a click event listener
+        buttonCell.appendChild(button);
       }
+    }
+  };
+  xhr.send();
+}
+
+// Click event handler for the button
+function createButtonClickHandler(item) {
+  return function() {
+
+    console.log(JSON.stringify(item));
+
+
+
+      var xhr = new XMLHttpRequest();
+      xhr.open("POST", "../../Model/edit-stock.php", true);
+      xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            // Handle the response from the PHP file
+            alert(xhr.responseText);
+        }
     };
-    xhr.send();
-  }
-  
+    };
+}
+
+
--- View/Admin/test.php
+++ View/Admin/test.php
@@ -1,17 +1,31 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Data Table Example</title>
-</head>
-<body>
+<table id="myTable">
+  <tr>
+    <th>Name</th>
+    <th>Email</th>
+    <th>Action</th>
+  </tr>
+  <tr>
+    <td>John Doe</td>
+    <td>johndoe@example.com</td>
+    <td><button onclick="editRow(this)">Edit</button></td>
+  </tr>
+  <tr>
+    <td>Jane Smith</td>
+    <td>janesmith@example.com</td>
+    <td><button onclick="editRow(this)">Edit</button></td>
+  </tr>
+  <!-- More rows... -->
+</table>
+
+<script>
+function editRow(button) {
+  // Get the row element
+  var row = button.parentNode.parentNode;
+
+  // Get the cell values within the row
+  var name = row.cells[0].innerHTML;
+  var email = row.cells[1].innerHTML;
 
 
-<div class="container">
-  <input type="number" id="editQuantity" placeholder="Edit quantity">
-
-</div>
-
-
-
-</body>
-</html>
+}
+</script>
