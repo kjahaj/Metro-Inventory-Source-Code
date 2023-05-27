c2d1117fe4acc034e341461e0a6b3e2359341b32
Eljon Zagradi <eljonzagradi@icloud.com>
2023-05-27 23:08:10 +0200

    FIX create-ticket

--- Model/create-ticket.php
+++ Model/create-ticket.php
@@ -5,6 +5,7 @@ $json = file_get_contents('php://input');
 $object = json_decode($json);
 
 // Access object properties
+$tittle = $object->tittle;
 $message = $object->message;
 $msgStatus = $object->{'msg-status'};
 $status = $object->status;
@@ -15,12 +16,12 @@ $senderID = $object->senderID;
 $userModifierID = $object->{'user-modifier-ID'};
 
 // Prepare the SQL query
-$sql = "INSERT INTO tickets (message, `msg-status`, `status`, `date-time-created`, `date-time-modified`, groupID, senderID, `user-modifier-ID`)
-        VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+$sql = "INSERT INTO tickets (tittle, message, `msg-status`, `status`, `date-time-created`, `date-time-modified`, groupID, senderID, `user-modifier-ID`)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
 // Bind the parameters
 $stmt = mysqli_prepare($conn, $sql);
-mysqli_stmt_bind_param($stmt, "ssssssss", $message, $msgStatus, $status, $dateTimeCreated, $dateTimeModified, $groupID, $senderID, $userModifierID);
+mysqli_stmt_bind_param($stmt, "sssssssss", $tittle, $message, $msgStatus, $status, $dateTimeCreated, $dateTimeModified, $groupID, $senderID, $userModifierID);
 
 // Execute the query
 if (mysqli_stmt_execute($stmt)) {
