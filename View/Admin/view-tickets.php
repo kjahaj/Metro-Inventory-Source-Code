a38ad1833df1f3f889429396fdfe44f9b70b969c
agoga21 <rurunikenshin1@gmail.com>
2023-05-31 19:42:57 +0200

    committed

--- View/Admin/view-tickets.php
+++ View/Admin/view-tickets.php
@@ -15,6 +15,161 @@ include '../../Model/session.php';
 </head>
 
 <body>
+    <style>
+#ticket-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 10vh;
+}
+
+.ticket {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 10px;
+    border: 1px solid #ccc;
+    margin-bottom: 10px;
+    width: 600px;
+}
+
+.left-container {
+    flex: 1;
+    display: flex;
+    align-items: center;
+}
+
+.title-status-container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.title {
+    margin-right: 10px;
+}
+
+.status {
+    font-weight: bold;
+}
+
+.right-container {
+    flex: 1;
+    text-align: right;
+}
+
+.label {
+    margin: 0;
+    color: #999;
+}
+
+.creation-date {
+    margin: 0;
+}
+
+.sender {
+    margin: 0;
+}
+
+.container {
+    display: flex;
+    justify-content: flex-end;
+}
+
+.container button {
+    padding: 10px 20px;
+    background-color: dodgerblue;
+    color: white;
+    border: none;
+    cursor: pointer;
+    max-width: 200px;
+}
+
+.popup {
+    display: none;
+    position: fixed;
+    z-index: 2;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    width: 400px;
+    padding: 30px;
+    background-color: #f9f9f9;
+    border-radius: 4px;
+    box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.2);
+    text-align: center;
+}
+
+.popup h2 {
+    margin-bottom: 20px;
+}
+
+.close {
+    position: absolute;
+    top: 10px;
+    right: 10px;
+    font-size: 24px;
+    color: #888;
+    cursor: pointer;
+}
+
+.close:hover {
+    color: #555;
+}
+
+.form-group {
+    margin-bottom: 20px;
+}
+
+label {
+    display: block;
+    font-weight: bold;
+}
+
+input[type="text"],
+textarea,
+select {
+    width: 100%;
+    padding: 8px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    font-size: 16px;
+}
+
+.popup textarea {
+    width: 100%;
+    max-width: 100%;
+    height: 150px;
+    max-height: 150px;
+    resize: vertical;
+    overflow-y: auto;
+}
+
+button[type="submit"] {
+    background-color: dodgerblue;
+    color: #fff;
+    padding: 10px 20px;
+    border: none;
+    border-radius: 4px;
+    font-size: 16px;
+    cursor: pointer;
+}
+
+
+
+#overlay {
+    display: none;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+    backdrop-filter: blur(5px);
+    z-index: 1;
+}
+    </style>
     <div class="sidebar" id='sidebar'>
         <div class="headeri">
             <button type="button" class="toggle" id="toggle">
@@ -37,9 +192,9 @@ include '../../Model/session.php';
             </div>
         </div>
         <ul>
-            <li><a href="#"><i class="fa fa-home" aria-hidden="true"></i> Dashboard</a></li>
+            <li><a href="index.php"><i class="fa fa-home" aria-hidden="true"></i> Dashboard</a></li>
             <br>
-            <li><a href="#"><i class="fas fa-clipboard-check" aria-hidden="true"></i> Check Stock</a></li>
+            <li><a href="stockcheck.php"><i class="fas fa-clipboard-check" aria-hidden="true"></i> Check Stock</a></li>
             <br>
             <li><a href="#"><i class="fas fa-money-bills" aria-hidden="true"></i> Buy Stock</a></li>
             <br>
@@ -47,7 +202,7 @@ include '../../Model/session.php';
             <br>
             <li><a href="#"><i class="fas fa-receipt" aria-hidden="true"></i> Check Requests</a></li>
             <br>
-            <li><a href="#"><i class="fa fa-sharp fa-light fa-ticket" aria-hidden="true"></i> View Tickets</a></li>
+            <li><a href="view-tickets.php"><i class="fa fa-sharp fa-light fa-ticket" aria-hidden="true"></i> View Tickets</a></li>
         </ul>
     </div>
 
